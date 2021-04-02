2841b8aeaea8576557e181571ae530a2d3c0a6ea
Jacob Piland <piland.jacob@gmail.com>
2021-04-01 23:25:06 -0500

    Add dinosaur model

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+dinosaur = {
+  "name" => "T-Rex",
+  "diet" => "Carnivore",
+  "height" =>  3.96, #meters
+  "weight" => 14, #metric tons
+  "is_nice" => false,
+  "would_kill" => true,
+  "other_names" => ["T. rex","T-Rex","The King"],
+  "food" => ["Triceratops", "Edmontosaurus","Anything They Wanted"]
+}
