b0acb5d4a54cfb421669d288db00689848ba5855
Jacob Piland <piland.jacob@gmail.com>
2021-04-01 23:33:23 -0500

    Add developer model

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+developer = {
+  "name" => "Jacob",
+  "confidence" => "Some",
+  "experience" => 0,
+  "free_time" => 0,
+  "is_good" => false,
+  "determined" => true,
+  "goals" => ["Keep learning", "Aske Questions", "Communicate"],
+  "mantra" => ["Practice", "Practice More", "Keep Practicing"]
+}
