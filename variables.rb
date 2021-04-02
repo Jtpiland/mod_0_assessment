c518c0c478c614b3e294aab4101ad519940a330c
Jacob Piland <piland.jacob@gmail.com>
2021-04-01 23:07:05 -0500

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,14 @@
+# Assigning Variables
+movie = "Star Wars"
+is_entertaining = true
+movie = "Wayne's World"
+num_4 = 1000
+num_6 = 5
+product = num_6 * num_4
+favorite_foods = ["pizza","ramen","burritos"]
+
+# Code removes first element from favorite_foods
+favorite_foods.delete_at(0)
+puts favorite_foods
+# Code prints how many characters are in movie variable
+puts movie.length
